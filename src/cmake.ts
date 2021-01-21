327c918bae7537cc9e981efb2aa52db5274862ec
sun-xm <sun-xm@hotmail.com>
2021-01-21 17:47:17 +0800

    cmake console update

--- src/cmake.ts
+++ src/cmake.ts
@@ -1,30 +1,33 @@
-import { TextEncoder } from 'util';
+import { TextDecoder, TextEncoder } from 'util';
 import * as vscode from 'vscode';
 
 export async function cmakeConsole(context:vscode.ExtensionContext) {
-    if (undefined != vscode.workspace.workspaceFolders) {
-        let ws  = vscode.workspace.workspaceFolders[0].uri.path;
+    if (undefined != vscode.workspace.name && undefined != vscode.workspace.workspaceFolders) {
+        let nm = vscode.workspace.name;
+        let ws = vscode.workspace.workspaceFolders[0].uri.path;
 
-        let cmake = `\
-cmake_minimum_required(VERSION 3.7)\n\
-project(${vscode.workspace.name})\n\
-add_executable(\${PROJECT_NAME} main.cpp)`
+        let uri: vscode.Uri;
+        let res = '/res/cmake/console';
 
-        let uri = vscode.Uri.file(ws + '/CMakeLists.txt');
-        await vscode.workspace.fs.writeFile(uri, new TextEncoder().encode(cmake));
-        vscode.window.showTextDocument(uri);
-
-        let cpp = '\
-#include "iostream"\n\n\
-int main(int argc, char* argv[])\n\
-{\n\
-    std::cout << "Hello World!";\n\
-}';
+        let decoder = new TextDecoder();
+        let encoder = new TextEncoder();
         
-        uri = vscode.Uri.file(ws + '/main.cpp');
-        await vscode.workspace.fs.writeFile(uri, new TextEncoder().encode(cpp));
+        let content: string;
+        content = decoder.decode(await vscode.workspace.fs.readFile(vscode.Uri.file(context.extensionUri.path + res + '/CMakeLists.txt')));
+        content = content.replace('__name__', nm);
+
+        uri = vscode.Uri.file(ws + '/CMakeLists.txt');
+        await vscode.workspace.fs.writeFile(uri, encoder.encode(content));
+
         vscode.window.showTextDocument(uri, { preview: false });
-        vscode.window.showInformationMessage(`CMake console project "${vscode.workspace.name}" created`)
+
+        content = decoder.decode(await vscode.workspace.fs.readFile(vscode.Uri.file(context.extensionUri.path + res + '/main.cpp')));
+
+        uri = vscode.Uri.file(ws + '/main.cpp');
+        await vscode.workspace.fs.writeFile(uri, encoder.encode(content));
+
+        vscode.window.showTextDocument(uri, { preview: false });
+        vscode.window.showInformationMessage(`CMake console project "${nm}" created`);
     }
     else {
         vscode.window.showErrorMessage('Workspace folder is undefined')
