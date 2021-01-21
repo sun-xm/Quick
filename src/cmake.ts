d1a2277875a1a38b363a0c34622c7cee06294f2b
sun-xm <sun-xm@hotmail.com>
2021-01-21 18:41:07 +0800

    cmake qt widgets

--- src/cmake.ts
+++ src/cmake.ts
@@ -1,7 +1,7 @@
 import { TextDecoder, TextEncoder } from 'util';
 import * as vscode from 'vscode';
 
-export async function cmakeConsole(context:vscode.ExtensionContext) {
+export async function cmakeConsole(context: vscode.ExtensionContext) {
     if (undefined != vscode.workspace.name && undefined != vscode.workspace.workspaceFolders) {
         let nm = vscode.workspace.name;
         let ws = vscode.workspace.workspaceFolders[0].uri.path;
@@ -21,13 +21,49 @@ export async function cmakeConsole(context:vscode.ExtensionContext) {
 
         vscode.window.showTextDocument(uri, { preview: false });
 
-        content = decoder.decode(await vscode.workspace.fs.readFile(vscode.Uri.file(context.extensionUri.path + res + '/main.cpp')));
+        await vscode.workspace.fs.copy(vscode.Uri.file(context.extensionUri.path + res + '/main.cpp'),
+                                       vscode.Uri.file(ws + '/main.cpp'), { overwrite: true });
 
-        uri = vscode.Uri.file(ws + '/main.cpp');
+        vscode.window.showInformationMessage(`CMake console project "${nm}" created`);
+    }
+    else {
+        vscode.window.showErrorMessage('Workspace folder is undefined')
+    }
+}
+
+export async function cmakeQtWidgets(context: vscode.ExtensionContext) {
+    if (undefined != vscode.workspace.name && undefined != vscode.workspace.workspaceFolders) {
+        let nm = vscode.workspace.name;
+        let ws = vscode.workspace.workspaceFolders[0].uri.path;
+
+        let uri: vscode.Uri;
+        let res = '/res/cmake/qtWidgets';
+
+        let decoder = new TextDecoder();
+        let encoder = new TextEncoder();
+        
+        let content: string;
+        content = decoder.decode(await vscode.workspace.fs.readFile(vscode.Uri.file(context.extensionUri.path + res + '/CMakeLists.txt')));
+        content = content.replace('__name__', nm);
+
+        uri = vscode.Uri.file(ws + '/CMakeLists.txt');
         await vscode.workspace.fs.writeFile(uri, encoder.encode(content));
 
         vscode.window.showTextDocument(uri, { preview: false });
-        vscode.window.showInformationMessage(`CMake console project "${nm}" created`);
+
+        await vscode.workspace.fs.copy(vscode.Uri.file(context.extensionUri.path + res + '/main.cpp'),
+                                       vscode.Uri.file(ws + '/main.cpp'), { overwrite: true });
+
+        await vscode.workspace.fs.copy(vscode.Uri.file(context.extensionUri.path + res + '/mainwindow.cpp'),
+                                       vscode.Uri.file(ws + '/mainwindow.cpp'), { overwrite: true });
+
+        await vscode.workspace.fs.copy(vscode.Uri.file(context.extensionUri.path + res + '/mainwindow.h'),
+                                       vscode.Uri.file(ws + '/mainwindow.h'), { overwrite: true });
+
+        await vscode.workspace.fs.copy(vscode.Uri.file(context.extensionUri.path + res + '/mainwindow.ui'),
+                                       vscode.Uri.file(ws + '/mainwindow.ui'), { overwrite: true });
+
+        vscode.window.showInformationMessage(`CMake Qt Widgets project "${nm}" created`);
     }
     else {
         vscode.window.showErrorMessage('Workspace folder is undefined')
--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,13 @@
 import * as vscode from 'vscode';
-import { cmakeConsole } from './cmake'
+import { cmakeConsole, cmakeQtWidgets } from './cmake'
 
 export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(vscode.commands.registerCommand('quick.cmakeConsole', ()=>{
 		cmakeConsole(context);
+	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand('quick.cmakeQtWidgets', ()=>{
+		cmakeQtWidgets(context);
 	}));
 }
 
