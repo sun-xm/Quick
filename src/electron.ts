6a6e06179e68236171ebfd698806cfc8cbc596eb
sun-xm <sun-xm@hotmail.com>
2021-09-18 14:06:01 +0800

    electron update

--- src/electron.ts
+++ src/electron.ts
@@ -24,22 +24,20 @@ export async function electronApp(context: vscode.ExtensionContext) {
         await copyText(`${res}/.vscode/launch.json_`, `${ws}/.vscode/launch.json`);
 
         await createFolder(`${ws}/css`);
-        await copyText(`${res}/css/about.css`, `${ws}/css/about.css`);
-        await copyText(`${res}/css/index.css`, `${ws}/css/index.css`);
-        await copyText(`${res}/css/menu.css`,  `${ws}/css/menu.css`);
-        await copyText(`${res}/css/title.css`, `${ws}/css/title.css`);
+        await copyText(`${res}/css/about.css`,  `${ws}/css/about.css`);
+        await copyText(`${res}/css/index.css`,  `${ws}/css/index.css`);
+        await copyText(`${res}/css/menu.css`,   `${ws}/css/menu.css`);
+        await copyText(`${res}/css/title.css`,  `${ws}/css/title.css`);
+        await copyText(`${res}/css/status.css`, `${ws}/css/status.css`);
         
         await createFolder(`${ws}/html`);
-        await copyText(`${res}/html/index.html`,        `${ws}/html/index.html`);
         await copyText(`${res}/html/about.html`,        `${ws}/html/about.html`);
-        await copyText(`${res}/html/about_title.html`,  `${ws}/html/about_title.html`);
-        await copyText(`${res}/html/content_img.html`,  `${ws}/html/content_img.html`);
-        await copyText(`${res}/html/content_mp4.html`,  `${ws}/html/content_mp4.html`);
+        await copyText(`${res}/html/index.html`,        `${ws}/html/index.html`);
+        await copyText(`${res}/html/title.html`,        `${ws}/html/title.html`);
         await copyText(`${res}/html/menu_color.html`,   `${ws}/html/menu_color.html`);
         await copyText(`${res}/html/menu_file.html`,    `${ws}/html/menu_file.html`);
         await copyText(`${res}/html/menu_help.html`,    `${ws}/html/menu_help.html`);
         await copyText(`${res}/html/menu_option.html`,  `${ws}/html/menu_option.html`);
-        await copyText(`${res}/html/index_title.html`,  `${ws}/html/index_title.html`, [[/__name__/g, nm]]);
 
         await createFolder(`${ws}/img`);
         await copyText(`${res}/img/check.svg`,      `${ws}/img/check.svg`);
@@ -53,16 +51,15 @@ export async function electronApp(context: vscode.ExtensionContext) {
         await copyText(`${res}/img/submenu.svg`,    `${ws}/img/submenu.svg`);
 
         await createFolder(`${ws}/src`);
-        await copyText(`${res}/src/about.ts_`,          `${ws}/src/about.ts`);
-        await copyText(`${res}/src/content_img.ts_`,    `${ws}/src/content_img.ts`);
-        await copyText(`${res}/src/content_mp4.ts_`,    `${ws}/src/content_mp4.ts`);
-        await copyText(`${res}/src/content.ts_`,        `${ws}/src/content.ts`);
-        await copyText(`${res}/src/dynamic.ts_`,        `${ws}/src/dynamic.ts`);
-        await copyText(`${res}/src/index_title.ts_`,    `${ws}/src/index_title.ts`);
-        await copyText(`${res}/src/index.ts_`,          `${ws}/src/index.ts`);
-        await copyText(`${res}/src/main.ts_`,           `${ws}/src/main.ts`);
-        await copyText(`${res}/src/menu.ts_`,           `${ws}/src/menu.ts`);
-        await copyText(`${res}/src/module.ts_`,         `${ws}/src/module.ts`);
+        await copyText(`${res}/src/about.ts_`,      `${ws}/src/about.ts`);
+        await copyText(`${res}/src/index.ts_`,      `${ws}/src/index.ts`);
+        await copyText(`${res}/src/title.ts_`,      `${ws}/src/title.ts`);
+        await copyText(`${res}/src/content.ts_`,    `${ws}/src/content.ts`);
+        await copyText(`${res}/src/status.ts_`,     `${ws}/src/status.ts`);
+        await copyText(`${res}/src/main.ts_`,       `${ws}/src/main.ts`);
+        await copyText(`${res}/src/menu.ts_`,       `${ws}/src/menu.ts`);
+        await copyText(`${res}/src/module.ts_`,     `${ws}/src/module.ts`);
+        await copyText(`${res}/src/dynamic.ts_`,    `${ws}/src/dynamic.ts`);
 
         vscode.window.showInformationMessage(`Electron project "${nm}" created. Run "npm install" to start`);
     }
