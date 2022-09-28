2bc1c0d301e33da70c038bac155ed4e64a441973
sun-xm <sun-xm@hotmail.com>
2022-09-28 11:37:44 +0800

    add designer path configuration

--- src/designer.ts
+++ src/designer.ts
@@ -2,5 +2,5 @@ import * as process from 'child_process';
 import * as vscode from 'vscode';
 
 export function openUiFile(uri: vscode.Uri) {
-    process.exec('designer ' + uri.fsPath);
+    process.exec(vscode.workspace.getConfiguration('quick').get<string>('designerPath') + ' ' + uri.fsPath);
 }
