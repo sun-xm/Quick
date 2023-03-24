b8477471b1f55f53f9cbae8d02407b8334512729
sun-xm <sun-xm@hotmail.com>
2023-03-24 17:32:06 +0800

    Update openUIFile

--- src/designer.ts
+++ src/designer.ts
@@ -1,6 +1,42 @@
 import * as process from 'child_process';
 import * as vscode from 'vscode';
 
-export function openUiFile(uri: vscode.Uri) {
-    process.exec(vscode.workspace.getConfiguration('quick').get<string>('designerPath') + ' ' + uri.fsPath);
+export async function openUiFile(uri: vscode.Uri) {
+    let designer = vscode.workspace.getConfiguration('quick').get<string>('designerPath')?.trim();
+
+    if (null == designer || 0 == designer.length) {
+        let paths = await vscode.window.showOpenDialog({ canSelectMany: false });
+        if (null != paths && paths.length > 0) {
+            designer = paths[0].fsPath;
+        }
+
+        if (null == designer || 0 == designer.length) {
+            vscode.window.showErrorMessage('Path to Qt Designer is not defined');
+            return;
+        }
+
+        let option = await vscode.window.showQuickPick([
+            { label: 'Save path to user settings', choice: 0 },
+            { label: 'Save path to workspace settings', choice: 1 },
+            { label: 'Do not save', choice: 2 }
+        ]);
+
+        switch (option?.choice) {
+            case 0: {
+                vscode.workspace.getConfiguration('quick').update('designerPath', designer, vscode.ConfigurationTarget.Global);
+                break;
+            }
+
+            case 1: {
+                vscode.workspace.getConfiguration('quick').update('designerPath', designer, vscode.ConfigurationTarget.Workspace);
+                break;
+            }
+
+            default: {
+                break;
+            }
+        }
+    }
+
+    process.exec(designer + ' ' + uri.fsPath);
 }
