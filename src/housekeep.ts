ac64df43ceef00c2f74c8ab63175adf9e1185037
sun-xm <sun-xm@hotmail.com>
2023-06-08 18:22:20 +0800

    housekeep cleanup on linux

--- src/housekeep.ts
+++ src/housekeep.ts
@@ -7,20 +7,33 @@ export async function cleanup() {
         return undefined;
     }
 
+    let promises: Promise<void>[] = [];
+
     switch (os.platform()) {
         case 'win32': {
-            return Promise.all([
-                new Promise<void>((resolve)=>{
-                    cp.exec('del /q .vscode\\VC.DB', { cwd: ws.first()!.uri.fsPath }, ()=>resolve());
-                }),
-                new Promise<void>((resolve)=>{
-                    cp.exec('rmdir /q /s .vscode\\ipch', { cwd: ws.first()!.uri.fsPath }, ()=>resolve());
-                })
-            ]);
+            promises.push(new Promise<void>((resolve)=>{
+                cp.exec('del /q .vscode\\VC.DB', { cwd: ws.first()!.uri.fsPath }, ()=>resolve());
+            }));
+            promises.push(new Promise<void>((resolve)=>{
+                cp.exec('rmdir /q /s .vscode\\ipch', { cwd: ws.first()!.uri.fsPath }, ()=>resolve());
+            }));
+            break;
+        }
+
+        case 'linux': {
+            promises.push(new Promise<void>((resolve)=>{
+                cp.exec('sleep 1s && rm -f .vscode/vc.db', { cwd: ws.first()!.uri.fsPath }, ()=>resolve());
+            }));
+            promises.push(new Promise<void>((resolve)=>{
+                cp.exec('sleep 1s && rm -rf .vscode/ipch', { cwd: ws.first()!.uri.fsPath }, ()=>resolve());
+            }));
+            break;
         }
 
         default: {
-            return undefined;
+            break;
         }
     }
+
+    return Promise.all(promises);
 }
